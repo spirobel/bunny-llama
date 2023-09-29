93facbf967242a992e82e2fb0385862a7a7fa7cf
Spirobel <spirobel@protonmail.com>
2023-09-29 18:35:11 +0800

    update gitignore, no special repo directory name

--- buntils/clone_llama.ts
+++ buntils/clone_llama.ts
@@ -3,12 +3,12 @@ const ENV = process.env;
 
 Bun.spawnSync({
 	cwd: path.join(import.meta.dir, ".."),
-	cmd: ["git", "clone", ENV.LLAMA_REPO_LINK!, "llama"],
+	cmd: ["git", "clone", ENV.LLAMA_REPO_LINK!],
 	stdio: ["inherit", "inherit", "inherit"],
 });
 
 Bun.spawnSync({
-	cwd: path.join(import.meta.dir, "../llama"),
+	cwd: path.join(import.meta.dir, "../llama.cpp"),
 	cmd: ["git", "reset", "--hard", String(ENV.LLAMA_COMMIT_HASH!)],
 	stdio: ["inherit", "inherit", "inherit"],
 });
