8e88a3d400831b330bd70de9005b5866814ba1ba
Spirobel <spirobel@protonmail.com>
2023-10-03 16:08:25 +0800

    more dev.ts tests

--- dev.ts
+++ dev.ts
@@ -4,9 +4,14 @@ import { global_llama } from "./bunny/example_init_functions/global_llama";
 function init(path: string) {
 	global_llama(path);
 	if (!llama._model) {
-		llama.load_model("llama --model models/model.gguf")
+		llama.load_model("llama --log-disable --model models/model.gguf --ctx-size 512")
 	}
+
+	console.log(llama._model)
 	llama.prompt("Hello this is a test")
+	console.log(llama.prompt("Hello this is a test"))
+	llama.prompt("Hello this is a test2")
+
 
 }
 bunny_hmr(init, "../llama.cpp/api-llama.so");
