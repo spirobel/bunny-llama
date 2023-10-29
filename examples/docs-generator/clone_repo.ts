8451c193aca0142975143a5e1b621cef2cdda284
Spirobel <spirobel@protonmail.com>
2023-10-29 11:17:39 +0800

    set example docs generator repo link

--- examples/docs-generator/clone_repo.ts
+++ examples/docs-generator/clone_repo.ts
@@ -1,5 +1,5 @@
 import * as path from "path";
-const REPO_LINK = "https://github.com/facebook/lexical"
+const REPO_LINK = "https://github.com/ggerganov/llama.cpp"
 
 Bun.spawnSync({
     cwd: path.join(import.meta.dir),
